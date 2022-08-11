e84375edcd070813f074b906cd871be4bba018e5
VtotheK <ville.keituri1@gmail.com>
2022-08-11 12:52:44 +0300

    asset handler logic

--- derelict/Assets/AssetHandler.cs
+++ derelict/Assets/AssetHandler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using derelict.Assets.Base;
@@ -7,9 +9,9 @@ namespace derelict.Assets
 {
     public class AssetHandler
     {
-        public List<Asset> Assets { get; set; }
-
         readonly string AssetDataPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Content");
+
+        public List<Asset> Assets { get; set; }
         private static Dictionary<string[], Action<string>> AssetHandlers = new Dictionary<string[], Action<string>>()
         {
             { new string[]{ ".png", ".jpeg", ".jpg"} , LoadSprite },
@@ -19,15 +21,31 @@ namespace derelict.Assets
 
         public void LoadAssetData()
         {
+            var files = Directory.GetFiles(AssetDataPath, "*", SearchOption.AllDirectories);
 
+            foreach (var filePath in files)
+            {
+                var fileExtension = Path.GetExtension(filePath);
+                foreach(var extensions in AssetHandlers.Keys)
+                {
+                    var extensionInKey = extensions
+                        .Where(extension => extension == fileExtension)
+                        .Any();
+                    if(extensionInKey)
+                    {
+                        AssetHandlers[extensions].Invoke(filePath);
+                    } 
+                }
+            }
         }
 
         private static void LoadSprite(string dsfas)
         {
+            Debug.WriteLine("Sprite loaded");
         }
         private static void LoadText(string obj)
         {
-
+            Debug.WriteLine("Text loaded");
         }
     }
 }
