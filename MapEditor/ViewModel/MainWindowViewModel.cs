4c821fa6339e009f3904609e4d9d7fc939be4d29
VtotheK <ville.keituri1@gmail.com>
2022-08-15 23:33:32 +0300

    editor improvements

---
+++ MapEditor/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using MapEditor.Model;
+
+namespace MapEditor.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public List<Sprite> Sprites;
+
+        public MainWindowViewModel()
+        {
+            Sprites = new List<Sprite>();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
